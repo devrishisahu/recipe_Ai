5673910ab0cd3c1948afee830237526155347923
devrishisahu <rishisahu9695@gmail.com>
2025-11-06 05:03:34 +0530

    preference-added

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ const addItemBtn = document.querySelector('#addItemBtn')
 const deleteBtn = document.querySelector('#deleteBtn')
 const loadingVideo = document.querySelector('#loadingVideo')
 const recipeResult = document.querySelector('#recipeResult')
+const toggleCheck = document.querySelector('#toggleCheck')
+const toggle = document.querySelector('#toggle')
+const vegItemOne = document.querySelector('#vegItemOne')
+const vegItemTwo = document.querySelector('#vegItemTwo')
+const vegItemThree = document.querySelector('#vegItemThree')
+const nonVegItemOne = document.querySelector('#nonVegItemOne')
+const nonVegItemTwo = document.querySelector('#nonVegItemTwo')
+const nonVegItemThree = document.querySelector('#nonVegItemThree')
+let preference
 
 const GEMINI_API_KEY = 'AIzaSyDt4Hw8etch2ZuvomrN6Buf8u5Hbbpr_J8';
 
@@ -28,8 +37,11 @@ function popOne() {
 }
 
 function emptyDisplay() {
+    console.log(toggleCheck.value)
+    console.log(toggle.value)
     display.value = ''
 }
+
 
 const addToDisplay = ((e) => {
 
@@ -39,19 +51,47 @@ const addToDisplay = ((e) => {
     addItemForm.reset()
 })
 
+function isCheacked() {
+
+    if (toggleCheck.checked) {
+        let preference = "non-veg"
+        console.log(preference)
+
+        vegItemOne.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden"
+        vegItemTwo.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden"
+        vegItemThree.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden"
+        nonVegItemOne.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black"
+        nonVegItemTwo.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black"
+        nonVegItemThree.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black"
+
+
+    } else {
+        let preference = "veg"
+        console.log(preference)
+        vegItemOne.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black "
+        vegItemTwo.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black "
+        vegItemThree.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black "
+        nonVegItemOne.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden "
+        nonVegItemTwo.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden "
+        nonVegItemThree.className = "bg-[#121f1f] rounded-4xl h-10 border border-[#2e3e3e] hover:bg-[#2e3e3b] text-white active:bg-white active:text-black hidden "
+
+    }
+
+}
+
 
 const cookFunction = ((eTwo) => {
     eTwo.preventDefault()
     recipeResult.innerText = "Our Miss recipe is cooking something for you .... "
     loadingVideo.style.display = "block"
     generateContent();
-    
+
 
 })
 
-
-
 async function generateContent() {
+
+    let preference
     try {
         const response = await fetch(
             "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
@@ -66,7 +106,7 @@ async function generateContent() {
                         {
                             parts: [
                                 {
-                                    text: `i have follwing ingredients ${display.value} suggest me some possible indian recipes which i can make using only these ingredients add the basic spices and some ingredients suggestions by your own and they must be veg and give the recipe data neat and without any '*','#' use the number labelling instead `,
+                                    text: `i have follwing ingredients ${display.value} suggest me some possible indian recipes which i can make using only these ingredients add the basic spices and some ingredients suggestions by your own and they must be ${preference} and give the recipe data neat and without any '*','#' use the number labelling instead `,
                                 },
                             ],
                         },
